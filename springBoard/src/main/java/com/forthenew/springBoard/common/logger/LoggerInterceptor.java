2a35cab8b2e8eddc0d23501f9ea1f83ca74f133d
forthenew <forthenew@forthenew-Desktop>
2016-07-28 07:19:12 +0900

    postHandle 변경. 기존 코드는 overriding이 되지 않아 인터셉터가 동작하지 않음.

--- springBoard/src/main/java/com/forthenew/springBoard/common/logger/LoggerInterceptor.java
+++ springBoard/src/main/java/com/forthenew/springBoard/common/logger/LoggerInterceptor.java
@@ -5,12 +5,14 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class LoggerInterceptor extends HandlerInterceptorAdapter {
 
 	protected Logger log = LoggerFactory.getLogger(LoggerInterceptor.class);
-	
+
+	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 		if( log.isDebugEnabled()) {
 			log.debug("======================================          START         ======================================");
@@ -19,9 +21,10 @@ public class LoggerInterceptor extends HandlerInterceptorAdapter {
 		return super.preHandle(request, response, handler);
 	}
 	
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
 		if (log.isDebugEnabled()) {
 			log.debug("======================================           END          ======================================\n");
-        }
+		}
 	}
 }
